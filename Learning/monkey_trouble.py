fcc18c2e620b9ca7397098144b5be3631a1910da
Wolfgang <kyriejpeg1890@gmail.com>
2026-02-05 22:26:07 -0600

    More coding

---
+++ Learning/monkey_trouble.py
@@ -0,0 +1,8 @@
+def monkey_trouble(a_smile, b_smile): #another codingbat warmup
+    if a_smile and b_smile:
+        return True
+    if not a_smile and not b_smile:
+        return True
+    return False
+
+#think more logically, try not to overthink using if cases, elif is not always necessary
---
+++ Learning/sleep_in.py
@@ -0,0 +1,8 @@
+def sleep_in(weekday, vacation): #coding bat practice
+    if not weekday or vacation:
+        return True
+    else:
+        return False
+
+#Utilize not and boolean inside a function
+#DO NOT CALL UNLESS INPUT IS NEEDED TO USE FUNCTION
